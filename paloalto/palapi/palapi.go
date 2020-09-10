976d19e7db83281d6b0d28184cd39bb6970e0cde
Alireza <alireza@cbt4all.com>
2020-09-10 23:10:03 +1200

    .

--- paloalto/palapi/palapi.go
+++ paloalto/palapi/palapi.go
@@ -3,20 +3,52 @@ package palapi
 import (
 	"crypto/tls"
 	"errors"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
-// TestRouteFibLookupApi generates REST URL to be used to get firewall interface information. This coresonds corresponds
-// the command 'test routing fib-lookup virtual-router <virtual-router> ip <ip-address>' in CLI but here is used on firewall REST API.
+// TestRouteFibLookupApi generates REST/XML URL to be used to get firewall FIB information. This coresonds corresponds
+// the command 'test routing fib-lookup virtual-router <virtual-router> ip <ip-address>' in CLI but here is used on firewall REST/XML API.
+// Output will be in XML/Jason format, depends on how the parameter 'api' is used.
+//
+// What this function dose is to call either testRouteFibLookupRST or testRouteFibLookupXML to generate REST/XML URL respectively. So if TestRouteFibLookupApi
+// is called by 'api=rest', testRouteFibLookupRST is used and if is called bu 'api=xml' testRouteFibLookupXML is called. The parameters are:
+// fip is Firewall IP
+// vr is Virtual-Router
+// dip is Destination IP
+// key is Token/Key should be takan manually from firewall
+// api is the API type you want to use. For now it can be either 'rest' or 'xml'
+func TestRouteFibLookup(fip, vr, dip, key, api string) ([]byte, error) {
+
+	switch api {
+	// To do: testRouteFibLookupRST
+	/*
+		case "rest":
+			{
+				return testRouteFibLookupRST(fip, vr, dip, key)
+			}
+	*/
+	case "xml":
+		{
+			return testRouteFibLookupXML(fip, vr, dip, key)
+		}
+	default:
+		{
+			return nil, errors.New("wrong type of API is used.")
+		}
+	}
+
+	return nil, errors.New("wrong type of API is used.")
+}
+
+// testRouteFibLookupXML generates an URL to be used to get firewall FIB information. This coresonds corresponds
+// the command 'test routing fib-lookup virtual-router <virtual-router> ip <ip-address>' in CLI but here is used on firewall XML API.
 // Output will be in XML format. In the command we have:
 // fip is Firewall IP
 // vr is Virtual-Router
 // dip is Destination IP
 // key is Token/Key should be takan manually from firewall
-func TestRouteFibLookupApi(fip, vr, dip, key string) []byte {
+func testRouteFibLookupXML(fip, vr, dip, key string) ([]byte, error) {
 
 	url := "https://" + fip + "/api/?type=op&cmd=<test><routing><fib-lookup><virtual-router>" + vr + "</virtual-router>"
 	url = url + "<ip>" + dip + "</ip></fib-lookup></routing></test>&key=" + key
@@ -31,34 +63,78 @@ func TestRouteFibLookupApi(fip, vr, dip, key string) []byte {
 
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		log.Fatal(resp.Status)
+		return nil, errors.New("Error in http connection:" + resp.Status)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
-	return body
+	return body, nil
 }
 
-// ShowInterfaceApi generates REST URL to be used to get firewall interface information. This coresonds corresponds
-// the command 'show interface <interface>' in CLI but here is used on firewall REST API.
+// testRouteFibLookupRST generates REST URL to be used to get firewall FIB information. This coresonds corresponds
+// the command 'test routing fib-lookup virtual-router <virtual-router> ip <ip-address>' in CLI but here is used on firewall REST API.
+// Output will be in XML format. In the command we have:
+// fip is Firewall IP
+// vr is Virtual-Router
+// dip is Destination IP
+// key is Token/Key should be takan manually from firewall
+func testRouteFibLookupRST(fip, vr, dip, key string) ([]byte, error) {
+	// To Do
+	// https://192.168.1.249/restapi/v9.1/Network/VirtualRouters
+	return nil, nil
+}
+
+// ShowInterface generates REST/XML URL to be used to get firewall interface information. This coresonds corresponds
+// the command 'show interface <interface>' in CLI but here is used on firewall REST/XML API.
+// Output will be in XML/Jason format, depends on how the parameter 'api' is used.
+// fip is Firewall IP
+// Intrfc is the interface we want
+// key is Token/Key should be takan manually from firewall
+// api is the API type you want to use. For now it can be either 'rest' or 'xml'
+func ShowInterface(fip, Intrfc, key, api string) ([]byte, error) {
+
+	switch api {
+	// To do: showInterfaceRST
+	/*
+		case "rest":
+			{
+				return showInterfaceRST(fip, Intrfc, key)
+			}
+	*/
+	case "xml":
+		{
+			return showInterfaceXML(fip, Intrfc, key)
+		}
+	default:
+		{
+			return nil, errors.New("wrong type of API is used.")
+		}
+	}
+
+	return nil, errors.New("wrong type of API is used.")
+
+}
+
+// showInterfaceXML generates XML URL to be used to get firewall interface information. This coresonds corresponds
+// the command 'show interface <interface>' in CLI but here is used on firewall XML API.
 // Output will be in XML format. In the command we have:
 // fip is Firewall IP
 // Intrfc is the interface we want
 // key is Token/Key should be takan manually from firewall
-func ShowInterfaceApi(fip, Intrfc, key string) []byte {
+func showInterfaceXML(fip, Intrfc, key string) ([]byte, error) {
 
 	url := "https://" + fip + "/api/?type=op&cmd=<show><interface>" + Intrfc
 	url = url + "</interface></show>&key=" + key
@@ -73,30 +149,43 @@ func ShowInterfaceApi(fip, Intrfc, key string) []byte {
 
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		log.Fatal(resp.Status)
+		return nil, errors.New("Error in http connection:" + resp.Status)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
-	return body
+	return body, nil
 }
 
-// TestSecurityPolicyMatchApi generates REST URL to be used to get firewall interface information. This coresonds corresponds
+// showInterfaceRST generates REST URL to be used to get firewall interface information. This coresonds corresponds
+// the command 'show interface <interface>' in CLI but here is used on firewall REST API.
+// Output will be in XML format. In the command we have:
+// fip is Firewall IP
+// Intrfc is the interface we want
+// key is Token/Key should be takan manually from firewall
+func showInterfaceRST(fip, vr, dip, key string) ([]byte, error) {
+	// To Do
+	//
+	return nil, nil
+}
+
+// TestSecurityPolicyMatch generates REST/XML URL to be used to get firewall policy match. This coresonds corresponds
 // the command 'test security-policy-match protocol 6 from <source-zone> to <destination-zone> source <source-ip-address>
 // destination <destination-ip-address> destination-port <destination-port> application <application-name>
+// Output will be in XML/Jason format, depends on how the parameter 'api' is used.
 // fip is Firewall IP
 // cfg is an 7-cell array in which we put these:
 // cfg[0] is Protocol Number (e.g. 6)
@@ -107,35 +196,64 @@ func ShowInterfaceApi(fip, Intrfc, key string) []byte {
 // cfg[5] is Destination Port
 // cfg[6] is Application
 // key is Token/Key should be takan manually from firewall
-func TestSecurityPolicyMatchApi(fip string, cfg [7]string, key string) ([]byte, error) {
+// api is the API type you want to use. For now it can be either 'rest' or 'xml'
+func TestSecurityPolicyMatch(fip string, cfg [7]string, key, api string) ([]byte, error) {
 
-	outErr := errors.New("nil")
-	outErr = nil
-	var result string
+	switch api {
+	// To do: testSecurityPolicyMatchRST
+	/*
+		case "rest":
+			{
+				return testSecurityPolicyMatchRST(fip, Intrfc, key)
+			}
+	*/
+	case "xml":
+		{
+			return testSecurityPolicyMatchXML(fip, cfg, key)
+		}
+	default:
+		{
+			return nil, errors.New("wrong type of API is used.")
+		}
+	}
+
+	return nil, errors.New("wrong type of API is used.")
+
+}
+
+// testSecurityPolicyMatchXML generates an URL to be used to get firewall interface information. This coresonds corresponds
+// the command 'test security-policy-match protocol 6 from <source-zone> to <destination-zone> source <source-ip-address>
+// destination <destination-ip-address> destination-port <destination-port> application <application-name>' in CLI but here is used on firewall XML API.
+// Output will be in XML format
+// fip is Firewall IP
+// cfg is an 7-cell array in which we put these:
+// cfg[0] is Protocol Number (e.g. 6)
+// cfg[1] is Source Zone
+// cfg[2] is Destination Zone
+// cfg[3] is Source IP
+// cfg[4] is Destination IP
+// cfg[5] is Destination Port
+// cfg[6] is Application
+// key is Token/Key should be takan manually from firewall
+func testSecurityPolicyMatchXML(fip string, cfg [7]string, key string) ([]byte, error) {
 
 	if cfg[0] == "" {
-		outErr = errors.New("Fist parameter should be protocol number!\n")
-		return []byte(""), outErr
+		return nil, errors.New("Fist parameter should be protocol number!\n")
 	}
 	if cfg[1] == "" {
-		outErr = errors.New("Second parameter should be Source Zone!\n")
-		return []byte(""), outErr
+		return nil, errors.New("Second parameter should be Source Zone!\n")
 	}
 	if cfg[2] == "" {
-		outErr = errors.New("Fist parameter should be Destination Zone!\n")
-		return []byte(""), outErr
+		return nil, errors.New("Fist parameter should be Destination Zone!\n")
 	}
 	if cfg[3] == "" {
-		outErr = errors.New("Fist parameter should be Source IP!\n")
-		return []byte(""), outErr
+		return nil, errors.New("Fist parameter should be Source IP!\n")
 	}
 	if cfg[4] == "" {
-		outErr = errors.New("Fist parameter should be Destination IP!\n")
-		return []byte(""), outErr
+		return nil, errors.New("Fist parameter should be Destination IP!\n")
 	}
 	if cfg[5] == "" {
-		outErr = errors.New("Fist parameter should be Destination Port!\n")
-		return []byte(""), outErr
+		return nil, errors.New("Fist parameter should be Destination Port!\n")
 	}
 
 	url := "https://" + fip + "/api/?type=op&cmd="
@@ -146,7 +264,7 @@ func TestSecurityPolicyMatchApi(fip string, cfg [7]string, key string) ([]byte, 
 	url = url + "<destination-port>" + cfg[5] + "</destination-port>"
 
 	if cfg[6] != "nil" {
-		result = result + " application "
+		url = url + " application "
 		url = url + "<application>" + cfg[6] + "</application>"
 	}
 
@@ -163,34 +281,58 @@ func TestSecurityPolicyMatchApi(fip string, cfg [7]string, key string) ([]byte, 
 
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		log.Fatal(resp.Status)
+		return nil, errors.New("Error in http connection:" + resp.Status)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
-	return body, outErr
-
+	return body, nil
 }
 
-// Get Object names
+// testSecurityPolicyMatchRST generates REST URL to be used to get firewall interface information. This coresonds corresponds
+// the command 'test security-policy-match protocol 6 from <source-zone> to <destination-zone> source <source-ip-address>
+// destination <destination-ip-address> destination-port <destination-port> application <application-name>' in CLI but here is used on firewall REST API.
+// Output will be in JSON format
+// fip is Firewall IP
+// cfg is an 7-cell array in which we put these:
+// cfg[0] is Protocol Number (e.g. 6)
+// cfg[1] is Source Zone
+// cfg[2] is Destination Zone
+// cfg[3] is Source IP
+// cfg[4] is Destination IP
+// cfg[5] is Destination Port
+// cfg[6] is Application
+// key is Token/Key should be takan manually from firewall
+func testSecurityPolicyMatchRST(fip string, cfg [7]string, key string) ([]byte, error) {
+	// To Do
+	//
+	return nil, nil
+}
+
+// REST Network
+// https://192.168.1.249/restapi/v9.1/Network/SDWANInterfaceProfiles
+// https://192.168.1.249/restapi/v9.1/Network/EthernetInterfaces?name=ethernet1/1
+// https://192.168.1.249/restapi/v9.1/Network/VirtualRouters?name=default
+
+// REST Get Object names
 // https://192.168.1.249/restapi/v9.1/Objects/Addresses?location=vsys&vsys=vsys1
 // https://192.168.1.249/restapi/v9.1/Objects/Addresses?location=vsys&vsys=vsys1&name=Eth1_1.1.1.254_24
 // https://192.168.1.249/restapi/v9.1/Objects/AddressGroups?location=vsys&vsys=vsys1&name=TestObject
 
-// Get Firewall Policy
+// REST Get Firewall Policy
 // https://192.168.1.249/restapi/v9.1/Policies/SecurityRules?location=vsys&vsys=vsys1&name=Z1-Z2
 // https://192.168.1.249/restapi/v9.1/Policies/SecurityRules?location=vsys&vsys=vsys1&source=HOST_10.1.0.1
 
