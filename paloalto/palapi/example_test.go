2b03d0b38052835fb120e2ec813335bdcab40da0
Alireza <alireza@cbt4all.com>
2020-09-27 20:56:26 +1300

    .

--- paloalto/palapi/example_test.go
+++ paloalto/palapi/example_test.go
@@ -99,6 +99,40 @@ func Example_showInterface() {
 	fmt.Println(string(b))
 }
 
+func Example_getPolicyMatch() {
+
+	// Create a new ClientSettings
+	cs, err := NewClientSettings(1, 1, "", "admin", "admin")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new Palo Alto Client
+	c, err := NewPClient(cs, "192.168.1.249", "", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var cfg [7]string
+	cfg[0] = "6"
+	cfg[1] = "ZONE1"
+	cfg[2] = "ZONE2"
+	cfg[3] = "192.168.0.1"
+	cfg[4] = "172.16.0.1"
+	cfg[5] = "22"
+	cfg[6] = "ssh"
+
+	// Get Action
+	b, err := c.GetPolicyMatch(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+	// Output:
+	// "deny"
+}
+
 func Example_testSecurityPolicyMatch() {
 
 	// Create a new ClientSettings
