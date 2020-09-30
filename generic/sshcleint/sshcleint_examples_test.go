9062d928fc5265c782bf40aafb8f10cc36991482
Alireza <alireza@cbt4all.com>
2020-09-30 16:52:14 +1300

    .

--- generic/sshcleint/sshcleint_examples_test.go
+++ generic/sshcleint/sshcleint_examples_test.go
@@ -1,13 +1,36 @@
 package sshcleint
 
+import (
+	"fmt"
+	"log"
+)
+
 func ExampleNewTClient() {
 
 	sshconfig := InsecureClientConfig("admin", "admin")
 
 	tc, err := NewTClient("192.168.1.1", sshconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//fmt.Println(cmd)
+	initcmd := []string{
+		"set cli op-command-xml-output on\n",
+	}
 
+	// List of the commands should be sent to the devices
+	listCMDs := []string{
+		"test routing fib-lookup virtual-router default ip 1.1.1.1\n",
+		"test routing fib-lookup virtual-router default ip 2.2.2.2\n",
+		"exit",
+	}
+
+	restult, err := tc.ExecCmds(initcmd, listCMDs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(restult)
 	// Output:
 	// test routing fib-lookup virtual-router default ip 192.168.1.1
 }
