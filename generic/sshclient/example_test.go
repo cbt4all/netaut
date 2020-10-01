6f167dd07092a72922e0dc0b5a043394142c8673
Alireza <alireza@cbt4all.com>
2020-10-01 22:55:55 +1300

    .

--- generic/sshclient/example_test.go
+++ generic/sshclient/example_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func Example_execCommands() {
+func Example_execCommandsAdvaned() {
 
 	// List of initial commands that dont need any outputs
 	initcmd := []string{
@@ -24,7 +24,7 @@ func Example_execCommands() {
 
 	rhc := CreateRhConfig("192.168.1.1", "tcp", "22")
 
-	result, err := ExecCommands(rhc, initcmd, listCMDs, sshconfig)
+	result, err := ExecCommandsAdvaned(rhc, initcmd, listCMDs, sshconfig)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,4 +34,25 @@ func Example_execCommands() {
 		fmt.Println(item.Result)
 	}
 
+}
+
+func Example_execCommandsSimple() {
+
+	// List of the commands should be sent to the devices
+	listCMDs := []string{
+		"test routing fib-lookup virtual-router default ip 1.1.1.1",
+		"show interface ethernet1/1 | match zone",
+		"exit",
+	}
+
+	sshconfig := InsecureClientConfig("admin", "admin", 5*time.Second)
+
+	rhc := CreateRhConfig("192.168.1.1", "tcp", "22")
+
+	result, err := ExecCommandsSimple(rhc, listCMDs, sshconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result)
 }
