e2901349b0563da070f3d8b46b84644558549d5b
Alireza <alireza@cbt4all.com>
2020-12-30 21:49:53 +1300

    .

--- paloalto/palapi/palapi_example_test.go
+++ paloalto/palapi/palapi_example_test.go
@@ -7,7 +7,7 @@ import (
 
 func ExamplePClient_getInterfaceFromFIB() {
 
-	// Create a new ClientSettings
+	// Create a new ClientSettings using username/password
 	cs, err := NewClientSettings(1, 1, "", "admin", "admin")
 	if err != nil {
 		log.Fatal(err)
